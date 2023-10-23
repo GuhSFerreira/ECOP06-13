ed9200424a7d8a4cecd821942d01bc49637abfc3
GuhSFerreira <146841525+GuhSFerreira@users.noreply.github.com>
2023-10-23 14:08:10 +0000

    Atualizdo

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
 
+st.set_page_config(
+    'Gustavo - ECOP06',
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+
+st.title('Página teste de ECOP06')
+
+esportes = pd.read_csv(
+    'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv',
+    encoding='latin-1')
+
+st.dataframe(esportes)
